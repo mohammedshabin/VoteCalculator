35dad1f28b35e1734bce4fd62fcdefc85e8576a1
Ron <ron2george@gmail.com>
2017-11-24 14:46:37 +0530

    Update vote.py

--- Code/vote.py
+++ Code/vote.py
@@ -1,63 +1,54 @@
+import operator
+
 print"---------------------------%_____________________%--------------------------------"
 
 print">>>>>>>>>>>>>>>>>>>>>>>>>>/VOTE COUNTING MACHINE\<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
 print"--------------------------------------------------------------------------------"
-n=input("ENTER THE NUMBER OF VOTERS IN THE AREA:")
-A=0
-B=0
-C=0
-D=0
-E=0
-a=raw_input("ENTER THE 1st CANDIDATE NAME: ")
-b=raw_input("\nENTER THE 2nd CANDIDATE NAME: ")
-c=raw_input("\nENTER THE 3rd CANDIDATE NAME: ")
-d1=raw_input("\nENTER THE 4th CANDIDATE NAME: ")
-e=raw_input("\nENTER THE 5th CANDIDATE NAME: ")
-i=1
-while i<=n:
- print "\n\nYOU ARE VOTER NUMBER",i,"\n\n"
- print"WHOM DO YOU WISH TO VOTE FOR \n\n1.",a,"  2.",b," 3.",c,"  4.",d1,"  5.",e," :"
- vote=input()
- if vote==1:
-  A=A+1
- elif vote==2:
-  B=B+1
- elif vote==3:
-  C=C+1
- elif vote==4:
-  D=D+1
- elif vote==5:
-  E=E+1
- else:
-  print"THERE IS NO SUCH CANDIDATE YOUR CHANCE IS OVER"
- i=i+1
+n = input("ENTER THE NUMBER OF VOTERS IN THE AREA:")
+candidates = input("ENTER THE NUMBER OF CANDIDATES:")
+CANDIDATES = []
+for i in range(candidates):
+    print "Enter the name of CANDIDATE No.", i + 1, ":"
+    CANDIDATES.append(raw_input())
+
+d = {}
+for k in range(candidates):
+    d[CANDIDATES[k]] = 0
+i = 1
+while i <= n:
+    print "\n\nYOU ARE VOTER NUMBER", i, "\n\n"
+    for k in range(candidates):
+        print k + 1, ". ", CANDIDATES[k]
+    print "YOU VOTE FOR:"
+
+    vote = input()
+
+    if vote <= candidates:
+        d[CANDIDATES[vote - 1]] += 1
+
+    else:
+        print"THERE IS NO SUCH CANDIDATE YOUR CHANCE IS OVER"
+    i = i + 1
 print"--------------------------------------------------------------------------------------------------------------------------------------\n"
 print"ELECTION RESULTS\n \n"
-f=1
-d={}
-d[a]=A
-d[b]=B
-d[c]=C
-d[d1]=D
-d[e]=E
+f = 1
+
+
 sorted((d.values()))
 print"\n\n"
-if A>B and A>C and A>D and A>E:
- print a,"IS THE ELECTION WINNER\n\n"
-elif B>C and B>D and B>E and B>A:
- print b,"IS THE ELECTION WINNER\n\n"
-elif C>D and C>E and C>A and C>B:
- print c,"IS THE ELECTION WINNER\n\n"
-elif D>E and D>A and D>B and D>C:
- print d,"IS THE ELECTION WINNER\n\n"
-elif E>A and E>B and E>C and E>D:
- print e,"IS THE ELECTION WINNER\n\n"
+z = max(d.iteritems(), key=operator.itemgetter(1))[0]
+same = -1
+for g in d:
+    if d[g] == d[z]:
+        same += 1
+if same < 1:
+    print"THE WINNER IS '", z, "'!!!!\n\n"
 else:
- print"SOME OF THE CANDIDATES HAS GOT SAME NUMBER OF VOTES.ELECTION IS NOT VALID" 
- f=0
-w=5
-for key, value in sorted(d.iteritems(), key=lambda (k,v): (v,k)):
- print"THE CANDIDATE",key,"AND HAS GOT",value,"NUMBER OF VOTES\n"
- if f==1:
-  print"AND IS AT POSITION",w,"\n\n\n" 
- w=w-1
+    print"SOME OF THE CANDIDATES HAS GOT SAME NUMBER OF VOTES.ELECTION IS NOT VALID"
+    f = 0
+w = candidates
+for key, value in sorted(d.iteritems(), key=lambda k_v: (k_v[1], k_v[0])):
+    print"THE CANDIDATE", key, "AND HAS GOT", value, "NUMBER OF VOTES\n"
+    if f == 1:
+        print"AND IS AT POSITION", w, "\n\n\n"
+        w = w - 1
